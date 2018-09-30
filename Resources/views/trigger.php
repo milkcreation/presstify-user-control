443eed7e592c172f2f6755dbdb3061dcacaa27df
Jordy Manner <jordy.manner@milkcreation.fr>
2018-09-30 20:36:04 +0000

    user-control -- tag 2.0.0 -- déploiement version bétâ

---
+++ Resources/views/trigger.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Lien de bascule.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Plugins\UserControl\Partial\UserControlPartialView $this .
+ */
+?>
+
+<?php
+echo partial(
+    'tag',
+    $this->all()
+);
