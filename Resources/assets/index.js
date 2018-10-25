10f9e56543dd520afde26e4d6a44a1387ae53157
Jordy Manner <jordy.manner@milkcreation.fr>
2018-10-25 06:32:56 +0000

    user-control -- tag 2.0.5 -- ajustements events

---
+++ Resources/assets/index.js
@@ -0,0 +1,10 @@
+/** Librairies tierces */
+import 'jquery';
+import 'presstify-framework/field/select-js/index';
+
+/** Sass */
+import './scss/styles.scss';
+
+/** Scripts */
+import './js/panel';
+import './js/switcher';
