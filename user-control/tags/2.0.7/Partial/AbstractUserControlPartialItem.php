dba6e666339d0706dcb8d0647277b8a5d462e5f8
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-11-27 08:08:23 +0000

    user-control -- tag 2.0.7 -- tify routing

---
+++ user-control/tags/2.0.7/Partial/AbstractUserControlPartialItem.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace tiFy\Plugins\UserControl\Partial;
+
+use tiFy\Partial\PartialController;
+use tiFy\Plugins\UserControl\UserControlResolverTrait;
+use tiFy\Plugins\UserControl\Contracts\UserControlPartialController;
+
+class AbstractUserControlPartialItem extends PartialController implements UserControlPartialController
+{
+    use UserControlResolverTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resourcesDir($path = '')
+    {
+        return $this->uc()->resourcesDir($path);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resourcesUrl($path = '')
+    {
+        return $this->uc()->resourcesUrl($path);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function viewer($view = null, $data = [])
+    {
+        if (!$this->viewer) :
+            $default_dir = $this->resourcesDir('views');
+            $this->viewer = view()
+                ->setDirectory($default_dir)
+                ->setController(UserControlPartialView::class)
+                ->setOverrideDir(
+                    (($override_dir = $this->get('viewer.override_dir')) && is_dir($override_dir))
+                        ? $override_dir
+                        : $default_dir
+                )
+                ->set('partial', $this);
+        endif;
+
+        if (func_num_args() === 0) :
+            return $this->viewer;
+        endif;
+
+        return $this->viewer->make("_override::{$view}", $data);
+    }
+}
