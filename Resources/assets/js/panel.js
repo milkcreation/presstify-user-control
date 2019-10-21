5eeb6672a9afe09f3ae435e23dc4e190fd8f3e3e
Jordy Manner <jordy.manner@milkcreation.fr>
2019-10-21 16:55:19 +0000

    user-control -- tag 2.0.17 --réécriture

---
+++ Resources/assets/js/panel.js
@@ -0,0 +1,29 @@
+'use strict';
+
+import jQuery from 'jquery';
+
+jQuery(document).ready(function ($) {
+    /**
+     * Ouverture/Fermeture.
+     */
+    $(document).on('click', '[aria-control="user_control-panel_toggle"]', function () {
+        let $panel = $($(this).data('target')),
+            visible = $panel.attr('aria-opened');
+
+        if (visible === 'false') {
+            $panel.attr('aria-opened', true);
+        } else {
+            $panel.attr('aria-opened', false);
+        }
+    });
+
+    /**
+     * Clic en dehors.
+     * @todo problème avec les liste de selection.
+    $(document).on('click', function (event) {
+        if (!$(event.target).closest('[aria-control="user_control-panel"][aria-opened="true"]').length) {
+            $('[aria-control="user_control-panel"][aria-opened="true"]').attr('aria-opened', false);
+        }
+    });
+     */
+});
