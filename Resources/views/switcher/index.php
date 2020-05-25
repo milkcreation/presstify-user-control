c7973866ebc7af1a9594887ca0de0b4360439b0c
Jordy Manner <jordy.manner@milkcreation.fr>
2020-05-25 10:09:58 +0000

    user-control -- 2.0.22 -- Compatibilité Bridge

---
+++ Resources/views/switcher/index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Formulaire de bascule.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Contracts\Partial\PartialView $this
+ */
+?>
+<div <?php echo $this->htmlAttrs($this->get('attrs', [])); ?>>
+    <form <?php echo $this->htmlAttrs($this->get('form', [])); ?>>
+        <?php wp_nonce_field('UserControlSwitcherForm', '_wpnonce', false); ?>
+
+        <?php echo field('hidden', [
+            'name'  => 'action',
+            'value' => 'switch',
+        ]); ?>
+
+        <?php echo field('hidden', [
+            'name'  => 'id',
+            'value' => $this->get('name'),
+        ]); ?>
+
+        <?php echo field('select-js', $this->get('role', [])); ?>
+
+        <?php echo field('select-js', $this->get('user', [])); ?>
+    </form>
+</div>
